a396a00949f98e65ccec49887f3cf3606f082bb3
mamc3334 <119633235+mamc3334@users.noreply.github.com>
2024-10-08 20:33:00 +0200

    Cmake build working  (#7)
    * Sample
    
    * Get CMakeBuild Build working

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # 18272-Project
 Fall 2024 Computer Architecture project on performance oriented programming. Members: Mason McGaffin, Finn Buggy, Ryan Biglou, Eloi Rodriguez Rodriguez
+
+Adding changes to the Readme file for example.
---
+++ cmake-build-debug/utest-imgaos/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/mamc3334/UC3M/18272-Project/utest-imgaos
+# Build directory: /home/mamc3334/UC3M/18272-Project/cmake-build-debug/utest-imgaos
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
