2f261d8f5f3c1edf1851ca73522fa6134433641f
100496948 <100496948@alumnos.uc3m.es>
2024-10-22 17:18:35 +0200

    merge

---
+++ common/intensityscaling.h
@@ -0,0 +1,8 @@
+//
+// Created by eloim on 22/10/2024.
+//
+
+#ifndef INTENSITYSCALING_H
+#define INTENSITYSCALING_H
+
+#endif //INTENSITYSCALING_H
--- imgaos/imageaos.cpp
+++ imgaos/imageaos.cpp
@@ -34,53 +34,9 @@ void aos_maxlevel()
         * Case 4: (hard) original > 255, new < 255
             * 6 bytes/pixel -> 3 bytes/pixel
             *
-/*test
-void read_image_IntensityScaling (string inputFilename, string outputFilename, int newIntensity) {
-    ifstream inputImageFile(inputFilename);
-    if(!inputImageFile.is_open()) {
-        cerr << "Failed to open input file\n";
-        exit(-1);
-    }
-    ofstream outputImageFile(outputFilename);
-    if(!outputImageFile.is_open()) {
-        cerr << "Failed to open output file\n";
-        exit(-1);
-    }
-    string magic_word, width, height, intensity;
-    inputImageFile >> magic_word >> width >> height >> intensity;
-    outputImageFile << magic_word << "\n" << width << "\n" << height << "\n" << intensity << "\n";
-    int colors = 3*width*height;
-    if(intensity <= 255){
-        if(newIntensity <= 255){
-            for(int i = 0;i < colors: i ++){
-                uint8_t x = read_binary8 (inputImageFile);
-                uint8_t newColor = (x*newIntensity)/intensity;
-                write_binary8 (outputImageFile, newColor);
-            }
-        }else{ //newIntensity > 255
-            for(int i = 0;i < colors: i ++){
-                uint8_t x = read_binary8 (inputImageFile);
-                uint16_t newColor = (x*newIntensity)/intensity;
-                write_binary16 (outputImageFile, newColor);
-            }
-        }
-    }else{ //intensity > 255
-        if(newIntensity <= 255){
-            for(int i = 0;i < colors: i ++){
-                uint16_t x = read_binary16 (inputImageFile);
-                uint8_t newColor = (x*newIntensity)/intensity;
-                write_binary8 (outputImageFile, newColor);
-            }
-        }else{ //newIntensity > 255
-            for(int i = 0;i < colors: i ++){
-                uint16_t x = read_binary16 (inputImageFile);
-                uint16_t newColor = (x*newIntensity)/intensity;
-                write_binary16 (outputImageFile, newColor);
-            }
-        }
-    }
-}
-     */
+            *
+            *
+   */
 }
 
 void aos_resize()
