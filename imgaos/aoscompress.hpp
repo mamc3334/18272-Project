24c189818923e8f87015442ccbb0359f08a10476
finnb <fbuggy@ucsb.edu>
2024-10-22 17:01:56 +0200

    Added compression source files

---
+++ imgaos/aoscompress.hpp
@@ -0,0 +1,17 @@
+//
+// Created by finnb on 10/22/2024.
+//
+
+#ifndef AOSCOMPRESS_HPP
+#define AOSCOMPRESS_HPP
+
+#include<iostream>
+#include<fstream>
+#include "../common/utility.hpp"
+
+using namespace std;
+
+
+
+
+#endif //AOSCOMPRESS_HPP
