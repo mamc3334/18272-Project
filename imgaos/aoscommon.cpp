a5d4074ce26cab11c1a881a252e94e2b8ad4d28f
finnb <fbuggy@ucsb.edu>
2024-10-22 17:30:19 +0200

    Separated color structs into a aoscommon file

---
+++ imgaos/aoscommon.cpp
@@ -0,0 +1,3 @@
+//
+// Created by finnb on 10/22/2024.
+//
---
+++ imgaos/aoscommon.hpp
@@ -0,0 +1,19 @@
+//
+// Created by finnb on 10/22/2024.
+//
+
+#ifndef AOSCOMMON_HPP
+#define AOSCOMMON_HPP
+#include <iostream>
+using namespace std;
+
+struct smallColor{
+    uint8_t r, g, b;
+};
+struct bigColor{
+    uint16_t r, g, b;
+};
+
+
+
+#endif //AOSCOMMON_HPP
