e79d920c876926408e05295ae599210bc1d230b3
RyanBiglou <ryan.biglou@gmail.com>
2024-10-22 10:42:29 -0700

    Preliminary CutFreq

--- imgaos/aosinfrequentcolor.cpp
+++ imgaos/aosinfrequentcolor.cpp
@@ -3,3 +3,24 @@
 //
 
 #include "aosinfrequentcolor.hpp"
+#include <vector>
+#include <iostream>
+using namespace std;
+//make a blank vector to store colors
+//iterate through pixels
+//if the pixel (the r, g, b value) is not in the vector, add it
+//if the pixel (the r, g, b value) is in the vector, increase it's count
+//sort the vector by count
+//replace the n least frequent (lowest count) pixels with the most similar pixel determined by euclidian (Sqrt((r2-r1)^2+((g2-g1)^2+(b2-b1)^2))
+struct color{
+ uint8_t int r, g, b;
+ int count;
+};
+
+vector <color> colors;
+
+//when reading (format)
+if (color in vector){
+
+int main(int n) {}
+#include "aosinfrequentcolor.hpp"
