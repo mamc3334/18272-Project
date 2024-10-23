4a2a49f67fe2abab9db1917e4287ba1ca503633f
Finn Buggy <fbuggy@ucsb.edu>
2024-10-23 13:35:20 +0200

    Wrote contains_color functions, rough version of get_colors

--- imgaos/aoscompress.cpp
+++ imgaos/aoscompress.cpp
@@ -4,6 +4,8 @@
 
 #include "aoscompress.hpp"
 #include "../common/utility.hpp"
+#include "../common/binaryio.hpp"
+#include <vector>
 
 using namespace std;
 
@@ -28,20 +30,45 @@ void compress(ifstream& infile, ofstream& outfile) {
     }
 }
 
+// Could combine these using some sort of template typename
+bool contains_small_color(vector<smallColor>& colors, smallColor& color) {
+	for(int i = 0; i < colors.size(); i++) {
+		if(colors[i] == color) {
+			return true;
+		}
+	}
+	return false;
+}
+
+bool contains_big_color(vector<bigColor>& colors, bigColor& color) {
+	for(int i = 0; i < colors.size(); i++) {
+		if(colors[i] == color) {
+			return true;
+		}
+	}
+	return false;
+}
 
 /*
 TODO:
+Pass in metadata to get width and height
 Read through all pixels of the input file
     On each read, check if the color exists in the color vector already
         If not, add that color to the vector
  */
 
-void get_small_colors(ifstream& infile, vector<smallColor>& colors) {
-
+void get_small_colors(ifstream& inFile, vector<smallColor>& colors, Image_Attributes& metadata) {
+	int numColors = 3*metadata.width*metadata.height;
+    for(int i = 0; i < numColors; i++) {
+        uint8_t color = read_binary8(inFile);
+		if(contains_small_color(colors, color)) {
+			colors.push_back(color);
+		}
+    }
 }
 
 
-void get_big_colors(ifstream& infile, vector<bigColor>& colors) {
+void get_big_colors(ifstream& infile, vector<bigColor>& colors, Image_Attributes& metadata) {
 
 }
 
--- imgaos/aoscompress.hpp
+++ imgaos/aoscompress.hpp
@@ -13,11 +13,15 @@
 using namespace std;
 
 void compress(ifstream &infile, ofstream &outfile);
+
+bool contains_big_color(vector<bigColor>& colors, bigColor& color);
+bool contains_small_color(vector<smallColor>& colors, smallColor& color);
+
 void get_small_colors(ifstream& infile, vector<smallColor>& colors);
 void get_big_colors(ifstream& infile, vector<bigColor>& colors);
+
 uint8_t getIndexByteLength(size_t colorSize);
+
 void write_small_colors(ifstream& infile, ofstream& outfile, Image_Attributes& metadata, vector<smallColor>& colors, uint8_t indexByteLength);
 void write_big_colors(ifstream& infile, ofstream& outfile, Image_Attributes& metadata, vector<bigColor>& colors, uint8_t indexByteLength);
-
-
 #endif //AOSCOMPRESS_HPP
