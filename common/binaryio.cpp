9ec7a3cb12fc3a1fcdea1121e278f7893d898c98
mamc3334 <119633235+mamc3334@users.noreply.github.com>
2024-10-23 16:44:17 +0200

    my attempt at read16 (#21)
    * my attempt at read16
    
    * Don't want to get rid of original function yet
    
    * added write function
    
    * bs

--- common/binaryio.cpp
+++ common/binaryio.cpp
@@ -16,10 +16,23 @@ std::uint16_t read_binary16 (std::istream & input) {
     return value;
 }
 
+std::uint16_t read_little_endian_binary16 (std::istream & input) {
+    std::uint8_t first = read_binary8(input);
+    std::uint8_t second = read_binary8(input);
+    std::uint16_t value = (second << 8) | first;
+    return value;
+}
+
 void write_binary16 (std::ostream & output, std::uint16_t const & value) {
     char const* two_bytes = reinterpret_cast<char const*>(&value);
     // NOLINTNEXTLINE(cppcoreguidelines−pro−type−reinterpret−cast)
     output.write(two_bytes, sizeof(value));
+}
+
+void write_little_endian_binary16 (std::ostream & output, std::uint16_t const & value)
+{
+    write_binary8(output, static_cast<uint8_t>(value&0xFF));
+    write_binary8(output, static_cast<uint8_t>(value>>8));
 }
 
 std::uint8_t read_binary8 (std::istream & input) {
