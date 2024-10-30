be916b9b3b17bc833c0d694aa7da791ba961067d
Mason <mamc3334@colorado.edu>
2024-10-30 17:53:12 +0100

    initial commit

---
+++ utest-imgaos/aossize_utest.cpp
@@ -0,0 +1,26 @@
+#include <cmake-build-debug/_deps/googletest-src/googletest/include/gtest/gtest.h>
+#include "../imgaos/imageaos.hpp"  // Include your common header file
+
+TEST(AOSSizeTests, Old16Test){
+
+}
+
+TEST(AOSSizeTests, Old8Test){
+
+}
+
+TEST(AOSSizeTests, Resize16Test) {
+
+}
+
+TEST(AOSSizeTests, Resize8Test) {
+
+}
+
+TEST(AOSSizeTests, Interpolate16Test) {
+
+}
+
+TEST(AOSSizeTests, Interpolate8Test) {
+
+}
