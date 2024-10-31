7225d37e371b3d1e1e9868fa0291c927b67e613d
Mason <mamc3334@colorado.edu>
2024-10-31 10:23:05 +0100

    work

--- utest-imgaos/aossize_utest.cpp
+++ utest-imgaos/aossize_utest.cpp
@@ -1,8 +1,19 @@
 #include <cmake-build-debug/_deps/googletest-src/googletest/include/gtest/gtest.h>
-#include "../imgaos/imageaos.hpp"  // Include your common header file
+#include "../imgaos/aossize.hpp"  // Include your common header file
 
 TEST(AOSSizeTests, Old16Test){
+    const unsigned int rows = 5;
+    const unsigned int cols = 7;
+    vector<vector<bigColor>> pixelArray(rows, vector<bigColor>(cols));
 
+    //TODO: create simple sample data image
+
+    aossize_old_photo_16(pixelArray, rows, cols, mockDataFile);
+
+    EXPECT_EQ(pixelArray[0][0], bigColor{10, 20, 30});
+    EXPECT_EQ(pixelArray[0][1], bigColor{40, 50, 60});
+    EXPECT_EQ(pixelArray[1][0], bigColor{70, 80, 90});
+    EXPECT_EQ(pixelArray[1][1], bigColor{100, 110, 120});
 }
 
 TEST(AOSSizeTests, Old8Test){
