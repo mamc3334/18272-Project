2d5c81ea8b2b64a8bac4cba3b12bc4195cca3a54
Mason <mamc3334@colorado.edu>
2024-11-04 14:57:44 +0100

    working on unit tests

---
+++ utest-common/utest-helpers.hpp
@@ -0,0 +1,13 @@
+//
+// Created by mcgaf on 11/4/2024.
+//
+
+#include <fstream>
+using namespace std;
+
+#ifndef UTEST_HELPERS_HPP
+#define UTEST_HELPERS_HPP
+
+bool compareBinaryFiles(const string& file1, const string& file2);
+
+#endif //UTEST_HELPERS_HPP
