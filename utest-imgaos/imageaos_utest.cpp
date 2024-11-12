1668e1c95cd65b0eac9655121bc94ecdb2cbf1c5
Mason <mamc3334@colorado.edu>
2024-11-12 15:24:12 +0100

    unit testing

--- utest-imgaos/imageaos_utest.cpp
+++ utest-imgaos/imageaos_utest.cpp
@@ -2,47 +2,47 @@
 #include "../imgaos/imageaos.hpp"  // Include your common header file
 #include "../common/binaryio.hpp"
 
-//Large 100 to 1000
-// TEST(ImageAOSTests, Resize16_100to1000_Test) {
-//   setInFile("../../test-data/lake-large-100.ppm");
-//   setOutFile("lake-large-output-1000.ppm");
-//   Image_Attributes newImageData = {.magic_word = "", .width=1000, .height=1000, .intensity = 0};
-//   aos_resize(newImageData);
-//
-//   const int result = system("diff -q ../../test-data/lake-large-1000.ppm lake-large-output-1000.ppm"); // NOLINT(*-env33-c)
-//   EXPECT_EQ(result, 0);
-// }
-//
-// //Large 1000 to 100
-// TEST(ImageAOSTests, Resize16_1000to100_Test) {
-//   setInFile("../../test-data/lake-large-1000.ppm");
-//   setOutFile("lake-large-output-100.ppm");
-//   Image_Attributes newImageData = {.magic_word = "", .width=100, .height=100, .intensity = 0};
-//   aos_resize(newImageData);
-//
-//   const int result = system("diff -q ../../test-data/lake-large-100.ppm lake-large-output-100.ppm"); // NOLINT(*-env33-c)
-//   EXPECT_EQ(result, 0);
-// }
-
-//Small 100 to 1000
-TEST(ImageAOSTests, Resize8_100to1000_Test) {
-  setInFile("../../test-data/lake-small-100.ppm");
-  setOutFile("lake-small-output-1000.ppm");
-  Image_Attributes newImageData = {.magic_word = "", .width=1000, .height=1000, .intensity = 0};
+//Large 2 to 4
+TEST(ImageAOSTests, Resize16_2to4_Test) {
+  setInFile("../../test-data/big16-2.ppm");
+  setOutFile("big16-4-output.ppm");
+  Image_Attributes newImageData = {.magic_word = "", .width=4, .height=4, .intensity = 1000};
   aos_resize(newImageData);
 
-  const int result = system("diff -q ../../test-data/lake-small-1000.ppm lake-small-output-1000.ppm"); // NOLINT(*-env33-c)
+  const int result = system("diff -q ../../test-data/big16-4.ppm big16-4-output.ppm"); // NOLINT(*-env33-c)
   EXPECT_EQ(result, 0);
 }
 
-//Small 100 to 1000
-TEST(ImageAOSTests, Resize8_1000to100_Test) {
-  setInFile("../../test-data/lake-small-1000.ppm");
-  setOutFile("lake-small-output-100.ppm");
-  Image_Attributes newImageData = {.magic_word = "", .width=100, .height=100, .intensity = 0};
+//Large 4 to 2
+TEST(ImageAOSTests, Resize16_4to2_Test) {
+  setInFile("../../test-data/big16-4.ppm");
+  setOutFile("big16-2-output.ppm");
+  Image_Attributes newImageData = {.magic_word = "", .width=2, .height=2, .intensity = 1000};
   aos_resize(newImageData);
 
-  const int result = system("diff -q ../../test-data/lake-small-100.ppm lake-small-output-100.ppm"); // NOLINT(*-env33-c)
+  const int result = system("diff -q ../../test-data/big16-2.ppm big16-2-output.ppm"); // NOLINT(*-env33-c)
+  EXPECT_EQ(result, 0);
+}
+
+//Small 2 to 4
+TEST(ImageAOSTests, Resize8_2to4_Test) {
+  setInFile("../../test-data/small8-2.ppm");
+  setOutFile("small8-4-output.ppm");
+  Image_Attributes newImageData = {.magic_word = "", .width=4, .height=4, .intensity = 255};
+  aos_resize(newImageData);
+
+  const int result = system("diff -q ../../test-data/small8-4.ppm small8-4-output.ppm"); // NOLINT(*-env33-c)
+  EXPECT_EQ(result, 0);
+}
+
+//Small 4 to 2
+TEST(ImageAOSTests, Resize8_4to2_Test) {
+  setInFile("../../test-data/small8-4.ppm");
+  setOutFile("small8-2-output.ppm");
+  Image_Attributes newImageData = {.magic_word = "", .width=2, .height=2, .intensity = 0};
+  aos_resize(newImageData);
+
+  const int result = system("diff -q ../../test-data/small8-2.ppm small8-2-output.ppm"); // NOLINT(*-env33-c)
   EXPECT_EQ(result, 0);
 }
 
