0aca188fe917b26b6b4dd725a0dc67c41399c1e7
Finn Buggy <fbuggy@ucsb.edu>
2024-11-12 12:11:08 +0100

    First unit test of compress

--- utest-imgaos/aoscompress_utest.cpp
+++ utest-imgaos/aoscompress_utest.cpp
@@ -17,4 +17,61 @@ TEST(AOSCompressTests, containsSmallColorTest){
     bool expectedFound = true;
 
     EXPECT_EQ(found, expectedFound);
+
+    targetColor = smallColor(7, 7, 7);
+    found = contains_smallColor(colors, targetColor);
+    expectedFound = false;
+
+    EXPECT_EQ(found, expectedFound);
+}
+
+TEST(AOSCompressTests, containsBigColorTest){
+    vector<bigColor> colors = {
+        bigColor(255, 0, 0),
+        bigColor(0, 255, 0),
+        bigColor(0, 0, 255),
+        bigColor(255, 0, 255),
+        bigColor(0, 255, 255),
+        bigColor(255, 0, 255)
+    };
+
+    bigColor targetColor = bigColor(0, 255, 0);
+    bool found = contains_bigColor(colors, targetColor);
+    bool expectedFound = true;
+
+    EXPECT_EQ(found, expectedFound);
+
+    targetColor = bigColor(7, 7, 7);
+    found = contains_bigColor(colors, targetColor);
+    expectedFound = false;
+
+    EXPECT_EQ(found, expectedFound);
+}
+
+TEST(AOSCompressTests, index_of_1bTest){
+    vector<smallColor> colors = {
+        smallColor(255, 0, 0),
+        smallColor(0, 255, 0),
+        smallColor(0, 0, 255),
+        smallColor(255, 0, 255),
+        smallColor(0, 255, 255),
+        smallColor(255, 0, 255)
+    };
+
+    smallColor targetColor = smallColor(0, 255, 0);
+    uint8_t index;
+    bool found = index_of_1b<smallColor>(colors, targetColor, index);
+    bool expectedFound = true;
+    uint8_t expectedIndex = 1;
+
+    EXPECT_EQ(found, expectedFound);
+    EXPECT_EQ(index, expectedIndex);
+
+    targetColor = smallColor(7, 7, 7);
+    found = index_of_1b<smallColor>(colors, targetColor, index);
+    expectedFound = false;
+    expectedIndex = 1;
+
+    EXPECT_EQ(found, expectedFound);
+    EXPECT_EQ(index, expectedIndex);
 }
