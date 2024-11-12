885a8e8add110a4fe3382efbdd7acb18b72c25e6
Mason <mamc3334@colorado.edu>
2024-11-12 15:53:14 +0100

    initial commit

---
+++ utest-imgsoa/soasize_utest.cpp
@@ -0,0 +1,10 @@
+#include <gtest/gtest.h>
+#include "../imgaos/aossize.hpp"
+#include "common/binaryio.hpp"
+#include <cstdlib>
+#include <filesystem>
+#include <fstream>
+
+TEST(SOASizeTests, Old16Test) {
+
+}
