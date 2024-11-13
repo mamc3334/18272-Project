58c92390d7301901bc76502deb64388e1751620d
mamc3334 <119633235+mamc3334@users.noreply.github.com>
2024-11-13 18:34:55 +0100

    ftests (#43)
    * initial commit
    
    * ALL UNIT TESTS WORK AND RUNS CORRECTLY
    
    * comments
    
    * commenting code and starting ftest-aos
    
    * ftests starter
    
    * comments and readme

--- imgsoa/imagesoa.cpp
+++ imgsoa/imagesoa.cpp
@@ -6,32 +6,45 @@
 #include <iostream>
 using namespace std;
 
+/* This is the general function that handles both cases for the soaresize using the input of the new width and height
+*/
 void soa_resize(Image_Attributes& NewImageData)
 {
+    // open input image
     ifstream imageFile(getInFile());
     if(!imageFile.is_open()) {
         cerr << "Failed to open file\n";
         exit(-1);
     }
+    // read input image metadata
     const Image_Attributes OldImageData = get_image_metadata(imageFile);
+    //open output stream
     ofstream outputImageFile(getOutFile());
     if(!outputImageFile.is_open()) {
         cerr << "Failed to open output file\n";
         exit(-1);
     }
+    //set new image metadata that is unchanged from user input
     NewImageData.magic_word = OldImageData.magic_word;
     NewImageData.intensity = OldImageData.intensity;
+    //print metadata to output file
     outputImageFile << NewImageData.magic_word << "\n" << NewImageData.width << " " << NewImageData.height << "\n" << NewImageData.intensity << "\n";
     if(NewImageData.intensity > IntensityCutoff)
     {
+        //r,g,b represented by uint16_t
         bigArray oldImage;
+        //read input image
         soasize_old_image_16(oldImage, OldImageData, imageFile);
+        //resize image and print output
         soasize_resize_16(oldImage, OldImageData, NewImageData, outputImageFile);
     }
     else
     {
+        //r,g,b represented by uint8_t
         smallArray oldImage;
+        //read input image
         soasize_old_image_8(oldImage, OldImageData, imageFile);
+        //resize image and print output
         soasize_resize_8(oldImage, OldImageData, NewImageData, outputImageFile);
     }
 }
--- imtool-soa/main.cpp
+++ imtool-soa/main.cpp
@@ -1,7 +1,6 @@
 //
 // Created by finnb on 10/7/2024.
 //
-#include <chrono>
 #include <iostream>
 #include <cstring>
 #include <span>
@@ -13,15 +12,17 @@ using namespace std;
 #include "../imgsoa/imagesoa.hpp"
 
 int main(int argc, char *argv[]) {
-  auto start = std::chrono::high_resolution_clock::now();
+    // all options require at least 4 args
     if (argc < 4) {
         cerr << "Error:\tInvalid number of arguments:\t" << argc << "\n";
         exit(-1);
     } else {
-        // Has the correct number of default arguments
+        // translate input args to be able to pass to progargs.
         span const args_view{ argv, static_cast<std::size_t>(argc) };
         vector<string> const args{args_view.begin() + 1, args_view.end()};
+        // set input and output files
         prog_paths(args[0], args[1]);
+        // process parameters based on user choice
         if(args[2] == "info")
         {
           prog_info(argc);
@@ -31,6 +32,7 @@ int main(int argc, char *argv[]) {
           read_image_intensity_scaling(stoi(args[3]));
         } else if (args[2] == "resize") {
           prog_resize(argc, args);
+          // create new image metadata and then resize
           Image_Attributes newImageData = {.magic_word = "",.width=static_cast<unsigned int>(stoi(args[3])), .height=static_cast<unsigned int>(stoi(args[4])), .intensity=0};
           soa_resize(newImageData);
         } else if (args[2] == "cutfreq") {
@@ -43,8 +45,5 @@ int main(int argc, char *argv[]) {
           cerr << "Error:\tInvalid option:\t" << args[2] << "\n";
           exit(-1);
         }
-      auto end = std::chrono::high_resolution_clock::now();
-      auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-      std::cout << "Time of execution: " << duration.count() << " microseconds" << "\n";
     }
 }
