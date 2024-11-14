a721d6ec8533d8e0e2ee996577c46fef16bff018
Mason <mamc3334@colorado.edu>
2024-11-15 00:21:11 +0100

    Functional Test

--- utest-imgaos/imageaos_utest.cpp
+++ utest-imgaos/imageaos_utest.cpp
@@ -5,56 +5,56 @@
 //Large 2 to 4
 TEST(ImageAOSTests, Resize16_2to4_Test) {
   // establish input and output locations and input metadata of new resized photo
-  setInFile("../../test-data/big16-2.ppm");
+  setInFile("../../expected-output/resize/big16-2.ppm");
   setOutFile("big16-4-output.ppm");
   Image_Attributes newImageData = {.magic_word = "", .width=4, .height=4, .intensity = 1000};
   aos_resize(newImageData);
 
   //diff command should have 0 exit code
   // NOLINTNEXTLINE(*-env33-c)
-  const int result = system("diff -q ../../test-data/big16-4.ppm big16-4-output.ppm");
+  const int result = system("diff -q ../../expected-output/resize/big16-4.ppm big16-4-output.ppm");
   EXPECT_EQ(result, 0);
 }
 
 //Large 4 to 2
 TEST(ImageAOSTests, Resize16_4to2_Test) {
   // establish input and output file locations and input metadata of resized photo
-  setInFile("../../test-data/big16-4.ppm");
+  setInFile("../../expected-output/resize/big16-4.ppm");
   setOutFile("big16-2-output.ppm");
   Image_Attributes newImageData = {.magic_word = "", .width=2, .height=2, .intensity = 1000};
   aos_resize(newImageData);
 
   //diff command should have 0 exit code
   // NOLINTNEXTLINE(*-env33-c)
-  const int result = system("diff -q ../../test-data/big16-2.ppm big16-2-output.ppm");
+  const int result = system("diff -q ../../expected-output/resize/big16-2.ppm big16-2-output.ppm");
   EXPECT_EQ(result, 0);
 }
 
 //Small 2 to 4
 TEST(ImageAOSTests, Resize8_2to4_Test) {
   // establish input and output file locations and input metadata of resized photo
-  setInFile("../../test-data/small8-2.ppm");
+  setInFile("../../expected-output/resize/small8-2.ppm");
   setOutFile("small8-4-output.ppm");
   Image_Attributes newImageData = {.magic_word = "", .width=4, .height=4, .intensity = 255};
   aos_resize(newImageData);
 
   //diff command should have 0 exit code
   // NOLINTNEXTLINE(*-env33-c)
-  const int result = system("diff -q ../../test-data/small8-4.ppm small8-4-output.ppm");
+  const int result = system("diff -q ../../expected-output/resize/small8-4.ppm small8-4-output.ppm");
   EXPECT_EQ(result, 0);
 }
 
 //Small 4 to 2
 TEST(ImageAOSTests, Resize8_4to2_Test) {
   // establish input and output file locations and input metadata of resized photo
-  setInFile("../../test-data/small8-4.ppm");
+  setInFile("../../expected-output/resize/small8-4.ppm");
   setOutFile("small8-2-output.ppm");
   Image_Attributes newImageData = {.magic_word = "", .width=2, .height=2, .intensity = 0};
   aos_resize(newImageData);
 
   //diff command should have 0 exit code
   // NOLINTNEXTLINE(*-env33-c)
-  const int result = system("diff -q ../../test-data/small8-2.ppm small8-2-output.ppm");
+  const int result = system("diff -q ../../expected-output/resize/small8-2.ppm small8-2-output.ppm");
   EXPECT_EQ(result, 0);
 }
 
