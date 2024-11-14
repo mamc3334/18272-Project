931aab651a3e0bc6c1799fb7590873b051701c26
Mason <mamc3334@colorado.edu>
2024-11-14 18:48:58 +0100

    avignon

--- avignon-tests/resize-stat.sh
+++ avignon-tests/resize-stat.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-perf stat build/imtool-aos/imtool-aos images/lake-small.ppm lake-small-resize-1000.ppm resize 1000 1000
-perf stat build/imtool-aos/imtool-aos images/lake-small.ppm lake-small-resize-100.ppm resize 100 100
-perf stat build/imtool-aos/imtool-aos images/lake-small.ppm lake-small-resize-8000.ppm resize 8000 8000
+# Append the file location
+FINAL_PATH="$(pwd)/images/lake-small.ppm"
+
+perf stat build/imtool-aos/imtool-aos "${FINAL_PATH}" lake-small-resize-1000.ppm resize 1000 1000
+perf stat build/imtool-aos/imtool-aos "${FINAL_PATH}" lake-small-resize-100.ppm resize 100 100
+perf stat build/imtool-aos/imtool-aos "${FINAL_PATH}" lake-small-resize-8000.ppm resize 8000 8000
