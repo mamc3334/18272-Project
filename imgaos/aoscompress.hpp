dab7bda80bfc4b4ac09ff75d0f61ff654590f2ff
fbuggy <115107219+fbuggy@users.noreply.github.com>
2024-11-15 14:36:47 +0100

    Equivalent SOA and AOS algorithms, optimized with maps (#49)
    * Progargs works, can call info and it will read the metadata.
    
    * Added compression source files
    
    * Separated color structs into a aoscommon file
    
    * Added validate_metadata function, haven't been able to fully test since main can't compile
    
    * Created framework for compression, implemented basic flow of functions
    
    * Wrote contains_color functions, rough version of get_colors
    
    * Added == operator for color structs
    
    * write_metadata, write_colors
    
    * Added full writing algorithm, still can't compile to test
    
    * Added newer files to imgaos/CMakeLists.txt
    
    * Bug fixes, still untested
    
    * Added write_binary32 for use in compression
    
    * Added == operator to big and small color in utility, include <cstdint> in both
    
    * premerge
    
    * Fixed type errors and removed validate_metadata, no errors in branch
    
    * Started working on unit tests
    
    * First unit test of compress
    
    * CMakeLists to work with gtest
    
    * Premerge
    
    * Completed multiple unit tests for compression, adjusted functions to work
    
    * Added read_binary32 to binaryio for testing purposes
    
    * All unit tests pass
    
    * fix pull
    
    * mason fix
    
    * new line
    
    * merges
    
    * merging
    
    * implemented unordered map algorithm, having difficulties with test files
    
    * Hashcodes
    
    * Minor edits for debugging
    
    * Edited function calls to compress
    
    * SOA Compress algorithm
    
    ---------
    
    Co-authored-by: Mason <mamc3334@colorado.edu>

--- imgaos/aoscompress.hpp
+++ imgaos/aoscompress.hpp
@@ -10,6 +10,7 @@
 #include "../common/utility.hpp"
 #include <cstdint>
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 
@@ -21,8 +22,8 @@ template <typename T> bool index_of_1b(vector<T> vec, T& element, uint8_t& index
 template <typename T> bool index_of_2b(vector<T> vec, T& element, uint16_t& index);
 template <typename T> bool index_of_4b(vector<T> vec, T& element, int& index);
 
-void get_small_colors(ifstream& inFile, vector<smallColor>& colors, unsigned int numPixels);
-void get_big_colors(ifstream& inFile, vector<bigColor>& colors, unsigned int numPixels);
+void get_small_colors(ifstream& inFile, vector<smallColor>& colors, unordered_map<smallColor, int>& colorIndexMap, unsigned int numPixels);
+void get_big_colors(ifstream& inFile, vector<bigColor>& colors, unordered_map<bigColor, int>& colorIndexMap, unsigned int numPixels);
 
 uint8_t getIndexByteLength(size_t colorSize);
 
@@ -30,15 +31,16 @@ void write_metadata(ofstream& outFile, Image_Attributes& metadata);
 void write_small_colors(ofstream& outFile, vector<smallColor>& colors);
 void write_big_colors(ofstream& outFile, vector<bigColor>& colors);
 
-void write_small_pixels(ifstream& inFile, ofstream& outFile, vector<smallColor>& colors, unsigned int numPixels, uint8_t indexByteLength);
-void write_small_pixels_1b(ifstream& inFile, ofstream& outFile, vector<smallColor>& colors, unsigned int numPixels);
-void write_small_pixels_2b(ifstream& inFile, ofstream& outFile, vector<smallColor>& colors, unsigned int numPixels);
-void write_small_pixels_4b(ifstream& inFile, ofstream& outFile, vector<smallColor>& colors, unsigned int numPixels);
+void write_small_pixels(ifstream& inFile, ofstream& outFile, unordered_map<smallColor, int>& colorIndexMap, unsigned int numPixels, uint8_t indexByteLength);
+void write_small_pixels_1b(ifstream& inFile, ofstream& outFile, unordered_map<smallColor, int>& colorIndexMap, unsigned int numPixels);
+void write_small_pixels_2b(ifstream& inFile, ofstream& outFile, unordered_map<smallColor, int>& colorIndexMap, unsigned int numPixels);
+void write_small_pixels_4b(ifstream& inFile, ofstream& outFile, unordered_map<smallColor, int>& colorIndexMap, unsigned int numPixels);
 
-void write_big_pixels(ifstream& inFile, ofstream& outFile, vector<bigColor>& colors, unsigned int numPixels, uint8_t indexByteLength);
-void write_big_pixels_1b(ifstream& inFile, ofstream& outFile, vector<bigColor>& colors, unsigned int numPixels);
-void write_big_pixels_2b(ifstream& inFile, ofstream& outFile, vector<bigColor>& colors, unsigned int numPixels);
-void write_big_pixels_4b(ifstream& inFile, ofstream& outFile, vector<bigColor>& colors, unsigned int numPixels);
-void write_big_pixels(ifstream& infile, ofstream& outfile, vector<bigColor>& colors, uint8_t numPixels);
+void write_big_pixels(ifstream& inFile, ofstream& outFile, unordered_map<bigColor, int>& colorIndexMap, unsigned int numPixels, uint8_t indexByteLength);
+void write_big_pixels_1b(ifstream& inFile, ofstream& outFile, unordered_map<bigColor, int>& colorIndexMap, unsigned int numPixels);
+void write_big_pixels_2b(ifstream& inFile, ofstream& outFile, unordered_map<bigColor, int>& colorIndexMap, unsigned int numPixels);
+void write_big_pixels_4b(ifstream& inFile, ofstream& outFile, unordered_map<bigColor, int>& colorIndexMap, unsigned int numPixels);
+void write_big_pixels(ifstream& infile, ofstream& outfile, unordered_map<bigColor, int>& colorIndexMap, uint8_t numPixels);
+// void uncompress(ifstream& inFile, ofstream& outFile);
 
 #endif //AOSCOMPRESS_HPP
